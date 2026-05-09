044f0bb6e82399b9b67457bba809b0648d258eb6
Victoriya Fadeeva <vic.f4deewa@gmail.com>
2026-05-09 11:46:50 +0300

    add project files

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeScreenTest.java
@@ -0,0 +1,34 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class HomeScreenTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testHomeFragmentText() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragment")
+                )
+        );
+    }
+}
